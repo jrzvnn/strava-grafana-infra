ffc4da56fb94eb195cad0dff06e8965de69a49fa
jrzvnn <jrzvnn@gmail.com>
2025-04-27 18:14:07 +0800

    feat: add nat gateway resource for private host internet access

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,3 +1,5 @@
+# VPC
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -8,6 +10,8 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Internet Gateway (for Public Subnets)
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -15,6 +19,8 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.project}-igw"
   }
 }
+
+# Subnets
 
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnets)
@@ -39,6 +45,27 @@ resource "aws_subnet" "private" {
   }
 }
 
+# NAT Gateway (for Private Subnets)
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${var.project}-nat"
+  }
+}
+
+# Route Tables
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -56,10 +83,17 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
   tags = {
     Name = "${var.project}-private-rt"
   }
 }
+
+# Route Table Associations
 
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnets)
